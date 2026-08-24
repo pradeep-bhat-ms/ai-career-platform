553c6ca8a040be568acb736f16be60ab68a7488b
prdeep bhat m s <pradeephsgr@gmail.com>
2026-08-24 14:54:41 +0530

    Initial commit: backend and frontend skeleton, Postgres+pgvector working, Spring AI Gemini connected

---
+++ backend/src/main/java/com/pradeep/aicareerplatform/AicareerplatformApplication.java
@@ -0,0 +1,14 @@
+package com.pradeep.aicareerplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class
+AicareerplatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AicareerplatformApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/pradeep/aicareerplatform/AicareerplatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pradeep.aicareerplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AicareerplatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
